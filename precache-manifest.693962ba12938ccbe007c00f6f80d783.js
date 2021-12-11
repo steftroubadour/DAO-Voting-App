e39213a0f380d8aaf9052b0b8cd0a43e4e97e66f
steftroubadour <steftroubadour@gmail.com>
2021-12-11 07:41:43 +0100

    Updates

---
+++ precache-manifest.693962ba12938ccbe007c00f6f80d783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc0893077adf88e8128ef40b86d4df8",
+    "url": "/Voting-App/index.html"
+  },
+  {
+    "revision": "a9996489067a99168513",
+    "url": "/Voting-App/static/css/2.3f075625.chunk.css"
+  },
+  {
+    "revision": "d614ef27790afcc5a45c",
+    "url": "/Voting-App/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a9996489067a99168513",
+    "url": "/Voting-App/static/js/2.dc5c3130.chunk.js"
+  },
+  {
+    "revision": "d614ef27790afcc5a45c",
+    "url": "/Voting-App/static/js/main.9da20b60.chunk.js"
+  },
+  {
+    "revision": "7d7c0a993ff802a3e51c",
+    "url": "/Voting-App/static/js/runtime-main.09723a0e.js"
+  }
+]);
